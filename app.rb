d1626d197f10557d94ab2e130f81985195cce291
Guillermo Esteves <guillermo.esteves@sbnation.com>
2014-06-27 11:40:25 -0400

    Yo

---
+++ app.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+require 'json'
+require 'httparty'
+
+get '/yo' do
+  username = params[:username]
+  if !username.nil?
+    text = "From #{username.upcase}"
+    payload = { :channel => ENV['CHANNEL'],
+                :text => text,
+                :username => 'YO',
+                :link_names => 1 }
+    uri = ENV['WEBHOOK']
+    response = HTTParty.post(uri, :body => { 'payload' => payload.to_json })
+    status 200
+    body 'Okay.'
+  else
+    status 400
+    body 'Not okay.'
+  end
+end
